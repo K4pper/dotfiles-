43a6176f5ab97f32a47ce19e5fd643000942b9e8
Kasper Therkelsen <kath@aura.dk>
2025-05-16 12:39:26 +0200

    more vim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,3 @@
-require('settings.options')
+require('config.options')
+require('config.lazy')
+require('config.keymaps')
---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,19 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.termguicolors = true
+
+vim.opt.cursorline = true
+
+vim.opt.ignorecase = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = false
+
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
