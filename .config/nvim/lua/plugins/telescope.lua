7a8cb41fc42ddd7c89eefb4fe55c03f2dd7487e9
Kasper Therkelsen <kath@aura.dk>
2025-06-06 08:01:58 +0200

    telescope: live_grep find hidden files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -5,6 +5,12 @@ return {
     require('telescope').setup {
         path_display = { 'smart' },
         pickers = {
+            live_grep = {
+              file_ignore_patterns = { 'node_modules', 'git' },
+              additional_args = function(_)
+                return { "--hidden" }
+              end
+            },
             find_files = {
                 find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' }
             }
