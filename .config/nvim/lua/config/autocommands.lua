e49babc8ff6512012e3ba09400257512d0e1f045
Kasper Therkelsen <kath@aura.dk>
2025-07-16 11:06:34 +0200

    trying lazyvim

--- .config/nvim/lua/config/autocommands.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    (vim.hl or vim.highlight).on_yank()
-  end,
-})
-
-vim.cmd [[ autocmd BufNewFile,BufRead *.bicep set filetype=bicep ]]
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,24 +1,3 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
-
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-
-vim.opt.clipboard = "unnamedplus"
-
-vim.opt.termguicolors = true
-
-vim.opt.cursorline = true
-
-vim.opt.ignorecase = true
-vim.opt.incsearch = true
-
-
-vim.opt.smartindent = true
-
-vim.opt.swapfile = false
-vim.opt.backup = false
-
-
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
